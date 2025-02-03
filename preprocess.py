ad8f005bdb0a0d9033e32adb6720cd1912f66c62
Yaning Wang <yanwa579@lnx00335.ad.liu.se>
2025-02-03 22:36:22 +0100

    test greyscale image, select 100 images,draw the probability plot

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,13 @@
+
 ################################################################################
-#zero padd, resize the images,and save images in images_resize
+#Zero padd, resize the images,and save images in images_resize
 #################################################################################
 
 
 # Check the image (greyscale or color)
 image_path = "/local/data1/yanwa579/Data/imageswithextraAFF_anonymized_8bit/patient_AAJEHHHPOU_NFF_image_1.png" 
 import os
-#install Pillow
+#Install Pillow
 from PIL import Image
 
 
@@ -44,9 +45,9 @@ def zeropad_and_resize(image):
     resized_image = cv2.resize(zeropadd_image, (256,256))
     return resized_image
 
-#use os.listdir to obtain a list of images' names
+#Use os.listdir to obtain a list of images' names
 for filename in os.listdir(input_folder):
-    #get the image path:   /local/data1/yanwa579/Data/../patient...png
+    #Get the image path:   /local/data1/yanwa579/Data/../patient...png
     image_path = os.path.join(input_folder, filename)   
     # Read the image
     image = cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
@@ -64,7 +65,7 @@ print(len(os.listdir(output_folder)))
 
 
 ################################################################################
-#split AFF and NFF and save them in AFF_resize and NFF_resize folders
+#Split AFF and NFF and save them in AFF_resize and NFF_resize folders
 #################################################################################
 
 import shutil
@@ -89,7 +90,7 @@ print(len(os.listdir(AFF_folder)))
 print(len(os.listdir(NFF_folder)))
 
 ##############################################
-#split the data into three dataset: AFF_train,AFF_valid,AFF_test
+#Split the data into three dataset: AFF_train,AFF_valid,AFF_test
 #NFF_train,NFF_valid,test,ratios are 70%,15%,15% respectively
 ##############################################
 
@@ -114,9 +115,9 @@ def spit_images(source_folder,train_folder,val_folder,test_folder):
     #Get the patient names like:{'DCAUHSLHGH': ['patient_DCAUHSLHGH_AFF_image_1.png']}
     patient_names = {}
     for image in image_files:
-        # split the name based on _ 
+        # Split the name based on _ 
         parts = image.split('_')
-        # filename format like: patient_id_AFF_image_1.png
+        # Filename format like: patient_id_AFF_image_1.png
         patient_name = parts[1]
         if patient_name not in patient_names:
             patient_names[patient_name] = []
@@ -142,7 +143,7 @@ def spit_images(source_folder,train_folder,val_folder,test_folder):
             for image in patient_images:
                 shutil.copy(os.path.join(source_folder, image), os.path.join(output_folder, image))
 
-    # copy images for each dataset
+    # Copy images for each dataset
     copy_patient_images(train_patients, train_folder)
     copy_patient_images(val_patients, val_folder)
     copy_patient_images(test_patients, test_folder)
@@ -158,7 +159,7 @@ print(f"NFF Train : {len(os.listdir(NFF_train_folder ))}, NFF Valid: {len(os.lis
 # From results obtained above:247 patients in AFF, 914 patients in NFF, in total: 1161 patiens
 #############
 
-#test the number of patiens in the imageswithextraAFF_anonmized_8bit
+#Test the number of patiens in the imageswithextraAFF_anonmized_8bit
 
 source_folder = '/local/data1/yanwa579/Data/imageswithextraAFF_anonymized_8bit'
 # Get all images'names
@@ -173,3 +174,138 @@ for image in image_files:
 #print(patient_names)
 print(f"Total patients: {len(patient_names)}")
 
+
+
+
+
+##############
+##Test if the image is greyscale
+#############
+
+
+import cv2
+import os
+image_path = "/local/data1/yanwa579/Data/imageswithextraAFF_anonymized_8bit/patient_AAJEHHHPOU_NFF_image_1.png" 
+
+image = cv2.imread(image_path)
+print(image.shape)
+
+
+import os
+from PIL import Image
+
+
+# Load the image from folder
+image = Image.open(image_path)
+# Convert mode to check grayscale
+if image.mode == "L":  # 'L' mode means grayscale in Pillow
+    print("The image is grayscale.")
+
+
+import cv2
+
+# Load the image in grayscale mode (single-channel)
+img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+# Check the shape
+print(img.shape)
+
+
+
+import cv2
+from PIL import Image
+# Load the image from folder
+image_path = "/local/data1/yanwa579/Data/AFF_train/patient_AOOBKUOKNK_AFF_image_1.png"
+image = Image.open(image_path)
+# Convert mode to check grayscale
+if image.mode == "L":  # 'L' mode means grayscale in Pillow
+    print("grayscale.")
+img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+print(img.shape)
+
+
+##############
+##Select 100 images from AFF_train folder used to test the code
+#############
+
+import os
+import shutil
+
+# Set source and destination folders
+source_folder = "/local/data1/yanwa579/Data/AFF_train"  
+destination_folder = "/local/data1/yanwa579/Data/testcode" 
+
+
+# Get list of image files
+image_files = [f for f in os.listdir(source_folder)]
+
+# Copy only the first 100 images
+for img in image_files[:100]:
+    src_path = os.path.join(source_folder, img)
+    dest_path = os.path.join(destination_folder, img)
+    shutil.copy(src_path, dest_path)
+
+###################
+#Count the number of patients
+#Draw the histogram show how many patient own how mang images
+###################
+import os
+
+# Define the source folder where images are stored
+source_folder = image_path = "/local/data1/yanwa579/Data/imageswithextraAFF_anonymized_8bit"
+# Get all images'names
+image_files =[f for f in os.listdir(source_folder)]
+#Extract unique patient name from the image names
+#Get the patient names like:{'DCAUHSLHGH': ['patient_DCAUHSLHGH_AFF_image_1.png']}
+patient_names = {}
+for image in image_files:
+    # Split the name based on _ 
+    parts = image.split('_')
+    # Filename format like: patient_id_AFF_image_1.png
+    patient_name = parts[1]
+    if patient_name not in patient_names:
+        patient_names[patient_name] = []
+    patient_names[patient_name].append(image)
+# Compute split sizes
+total_patients = len(patient_names)
+# Get all the unique patient names
+patient_list = list(patient_names.keys())
+patients = {}
+for patient in patient_list:
+    patients[patient] = len(patient_names[patient])
+
+print(f"Total patients: {total_patients}")
+print(patients)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+values = list(patients.values())
+plt.hist(values, bins=10,color='skyblue', edgecolor='black', alpha=0.7, density=True)
+# Add trend line
+sns.kdeplot(values, color='red', linewidth=2)
+# Labels and title
+plt.xlabel("the number of images per patient")
+plt.ylabel("Frequency")
+plt.show()
+max(patients.values())
+
+############
+#Draw the image size distribution
+############
+
+image_sizes = []
+#Use os.listdir to obtain a list of images' names
+for filename in os.listdir(source_folder):
+    #Get the image path:   /local/data1/yanwa579/Data/../patient...png
+    image_path = os.path.join(source_folder, filename)   
+    # Read the image
+    image = cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
+    image_sizes.append(image.size)
+
+plt.hist(image_sizes, bins=100, color='skyblue', edgecolor='black', alpha=0.7, density=True)
+# Add trend line
+sns.kdeplot(image_sizes, color='red', linewidth=2)
+plt.xlabel("image size")
+plt.ylabel("Frequency")
+plt.show()
